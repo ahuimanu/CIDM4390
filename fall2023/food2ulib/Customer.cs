24ea8a6fb0989c96193ae415c01bed4ea45eb19f
Jeffry Babb <ahuimanu@gmail.com>
2023-11-15 21:10:07 -0600

    updated to net7

---
+++ fall2023/food2ulib/Customer.cs
@@ -0,0 +1,7 @@
+﻿namespace food2ulib;
+
+public class Customer
+{
+    public string FirstName { get; set; } //implicit variable / class member
+
+}
---
+++ fall2023/temp/food2ulib/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace food2ulib;
+
+// I used the following command to create this class:
+public class Customer
+{
+    
+
+}
